8e6affa181ba7adaefa2812cada7e2b8725e000c
lior.benzvi <lior.benzvi@start.io>
2021-12-25 14:28:08 +0200

    parameter_tuning

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import RandomizedSearchCV
 
 
 def clean_data(df):
@@ -20,7 +21,8 @@ def clean_data(df):
                   'also', 'cream', 'was', 'bbq', 'the', 'and', 'product', 'had', 'this', 'would', 'should', 'thought',
                   'for', 'box', 'tast', 'what', 'make', 'flavor', 'money', 'receiv', 'hope', 'have', 'were', 'review',
                   'are', 'too', 'has', 'gave', 'find', 'year', 'compani', 'their', 'buy', 'time', 'you', 'way', 'deal',
-                  'almond', 'list', 'that', 'need', 'quit', 'again', 'been', 'wonder', 'about', 'them'], axis=1)
+                  'almond', 'list', 'that', 'need', 'quit', 'again', 'been', 'wonder', 'about', 'them',
+                  'happen', 'idea', 'sound', 'later', 'stevia', 'guess', 'with'], axis=1)
     return df
 
 
@@ -33,14 +35,20 @@ def get_xy(df):
 
 def print_feature_importance(model):
     model.fit(x, y)
-    if hasattr(model, 'feature_importances_'):
-        importance = model.feature_importances_
+    if hasattr(model, 'feature_importances_') or hasattr(model, 'coef_'):
+        if hasattr(model, 'feature_importances_'):
+            importance = model.feature_importances_
+        else:
+            importance = model.coef_
         sorted_indices = np.argsort(importance)[::-1]
         print('20 most important features (sorted by importance): ')
         sorted_features = x.columns[sorted_indices]
-        print(*sorted_features[:20], sep="\n")
+        if hasattr(model, 'feature_importances_'):
+            print(*sorted_features[:20], sep="\n")
+        else:
+            print(*sorted_features[0][:20], sep="\n")
     else:
-        print('model does not have feature_importances_ so skipping this method')
+        print('model does not have feature_importances_ or coef_ so skipping this method')
 
 
 def train_and_evaluate_model(model, x, y, cv):
@@ -49,14 +57,10 @@ def train_and_evaluate_model(model, x, y, cv):
     print_feature_importance(model)
 
 
-if __name__ == '__main__':
-    df = pd.read_csv("csv_files/text_training.csv", encoding="UTF-8")
-    df = clean_data(df)
-    cv = KFold(n_splits=10, random_state=1, shuffle=True)
-    x, y = get_xy(df)
+def choose_best_model(df, cv, x, y):
     print('going to check different models:')
     best_features = ['not', 'great', 'return', 'disappoint', 'love', 'wast', 'perfect', 'best', 'bad', 'terribl',
-                    'horribl', 'threw', 'worst', 'favorit', 'disgust', 'wouldnt', 'refund', 'good', 'stale', 'noth']
+                     'horribl', 'threw', 'worst', 'favorit', 'disgust', 'wouldnt', 'refund', 'good', 'stale', 'noth']
     models = [('RF', RandomForestClassifier()),
               ('SVM', svm.SVC()),
               ('LR', LogisticRegression()),
@@ -69,11 +73,46 @@ if __name__ == '__main__':
     for name, m in models:
         print(f'\n{name}:')
         train_and_evaluate_model(m, x, y, cv)
-        print('now again only with best feature')
-        train_and_evaluate_model(m,
-                                 df[best_features],
-                                 df[['rating']].values.ravel(),
-                                 cv)
+        # print('now again only with best feature')
+        # train_and_evaluate_model(m, df[best_features], df[['rating']].values.ravel(), cv)
+
+
+def parameter_tuning(x, y):
+    random_grid = {'n_estimators': [int(x) for x in np.linspace(start=200, stop=2000, num=10)],
+                   'max_features': ['auto', 'sqrt'],
+                   'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
+                   'min_samples_split': [2, 5, 10],
+                   'min_samples_leaf': [1, 2, 4],
+                   'bootstrap': [True, False]}
+    rf = RandomForestClassifier()
+    rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_grid, n_iter=100, cv=3,
+                                   verbose=2, random_state=42, n_jobs=-1)
+    rf_random.fit(x, y)
+    print('\nBest params: ')
+    print(rf_random.best_params_)
+
+
+def calc_improvement(x, y):
+    base_model = RandomForestClassifier()
+    base_accuracy = cross_val_score(base_model, x, y, scoring='accuracy', cv=cv, n_jobs=-1)
+    print('Base accuracy: %.3f (%.3f)' % (mean(base_accuracy), std(base_accuracy)))
+    # best_param = {'n_estimators': 2000, 'min_samples_split': 5, 'min_samples_leaf': 1, 'max_features': 'auto',
+    #               'max_depth': None, 'bootstrap': False}
+    best_random = RandomForestClassifier(n_estimators=2000, min_samples_split=5, min_samples_leaf=1, max_features='auto'
+                                         , max_depth=None, bootstrap=False)
+    improved_accuracy = cross_val_score(best_random, x, y, scoring='accuracy', cv=cv, n_jobs=-1)
+    print('Best accuracy: %.3f (%.3f)' % (mean(improved_accuracy), std(improved_accuracy)))
+    print('Improvement of {:0.2f}%.'.format(100 * (mean(improved_accuracy) - mean(base_accuracy)) / mean(base_accuracy)))
+
+
+if __name__ == '__main__':
+    df = pd.read_csv("csv_files/text_training.csv", encoding="UTF-8")
+    df = clean_data(df)
+    cv = KFold(n_splits=10, random_state=1, shuffle=True)
+    x, y = get_xy(df)
+    # choose_best_model(df, cv, x, y)
+    # parameter_tuning(x, y)
+    # calc_improvement(x, y)
 
 
 
